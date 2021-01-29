56a18bed6595adf82b4b9e3c756b0062b9992a80
darkhz <kmachanwenw@gmail.com>
2021-01-29 10:47:33 +0530

    sched/fair: Modify boosted_task_util() to reflect uclamp changes
    This is a commit that reflects:
    ANDROID: sched/fair: EAS: Add uclamp support to find_energy_efficient_cpu()
    
    https: //github.com/neverware/kernel/commit/3a5e1534e01421a455df2d3f4daaef1b6da54f30
    Change-Id: Ifde657700df1178e7484f0c5768363218cc0c47a

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6841,12 +6841,20 @@ boosted_cpu_util(int cpu, struct sched_walt_cpu_load *walt_load)
 static inline unsigned long
 boosted_task_util(struct task_struct *task)
 {
+#ifdef CONFIG_UCLAMP_TASK_GROUP
+	unsigned long util = task_util_est(task);
+	unsigned long util_min = uclamp_eff_value(task, UCLAMP_MIN);
+	unsigned long util_max = uclamp_eff_value(task, UCLAMP_MAX);
+
+	return clamp(util, util_min, util_max);
+#else
 	unsigned long util = task_util_est(task);
 	long margin = schedtune_task_margin(task);
 
 	trace_sched_boost_task(task, util, margin);
 
 	return util + margin;
+#endif
 }
 
 static unsigned long cpu_util_without(int cpu, struct task_struct *p);
