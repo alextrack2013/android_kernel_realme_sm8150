f089771654f3bf442dd149b1461683d979c5d22b
karthick111 <karthickrko61@gmail.com>
2021-11-16 21:04:35 +0530

    drivers: scsi: Import UFS changes from Realme
    Change-Id: I059e9375ac0f5edf21dcd9e837a061e7206f5549

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -15,6 +15,11 @@
 #include <linux/swap.h>
 #include <linux/kthread.h>
 
+#if defined(VENDOR_EDIT) && defined(CONFIG_UFSTW)
+/* Hank.liu@TECH.PLAT.Storage, 2019-10-31, add UFS+ hpb and tw driver*/
+#include <linux/ufstw.h>
+#endif
+
 #include "f2fs.h"
 #include "node.h"
 #include "segment.h"
@@ -1620,6 +1625,10 @@ int f2fs_write_checkpoint(struct f2fs_sb_info *sbi, struct cp_control *cpc)
 	if (cpc->reason != CP_RESIZE)
 		f2fs_down_write(&sbi->cp_global_sem);
 
+#if defined(VENDOR_EDIT) && defined(CONFIG_UFSTW)
+/* Hank.liu@TECH.PLAT.Storage, 2019-10-31, add UFS+ hpb and tw driver*/
+        bdev_set_turbo_write(sbi->sb->s_bdev);
+#endif
 	if (!is_sbi_flag_set(sbi, SBI_IS_DIRTY) &&
 		((cpc->reason & CP_FASTBOOT) || (cpc->reason & CP_SYNC) ||
 		((cpc->reason & CP_DISCARD) && !sbi->discard_blks)))
@@ -1698,6 +1707,10 @@ stop:
 	f2fs_update_time(sbi, CP_TIME);
 	trace_f2fs_write_checkpoint(sbi->sb, cpc->reason, "finish checkpoint");
 out:
+#if defined(VENDOR_EDIT) && defined(CONFIG_UFSTW)
+/* Hank.liu@TECH.PLAT.Storage, 2019-10-31, add UFS+ hpb and tw driver*/
+	bdev_clear_turbo_write(sbi->sb->s_bdev);
+#endif
 	if (cpc->reason != CP_RESIZE)
 		f2fs_up_write(&sbi->cp_global_sem);
 	return err;
